e7f2383c2325187d2f4a646f108a31b48dfd6949
OnedariDaisakusen <novagit666@gmail.com>
2020-08-05 19:27:30 -0500

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//rutas
+import { APPROUTING } from './app.routes';
+
+
+
+//servicios
+import { HerosService } from './services/heros.service';
+
+
+//componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HerosComponent } from './components/heros/heros.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HerosComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APPROUTING
   ],
-  providers: [],
+  providers: [
+    HerosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
